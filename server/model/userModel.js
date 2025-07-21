e072cc8a55d544abba875ceb8864ddbe4a7aceb0
David Chung <dbchung24@gmail.com>
2025-07-20 20:33:51 -0700

    first commit

---
+++ server/model/userModel.js
@@ -0,0 +1,22 @@
+import mongoose, { mongo } from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    year: {
+        type: Number,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    address: {
+        type: String,
+        required: true,
+    },
+})
+
+export default mongoose.model("Users", userSchema);
